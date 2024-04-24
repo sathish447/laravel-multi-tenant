7c558026a87904fe2cdd434c0249ca4c66b5e22a
sathish447 <sathishprabu447@gmail.com>
2024-04-24 11:37:39 +0530

    multiple tenant login with same domain different host

---
+++ README.md
@@ -0,0 +1,25 @@
+
+## Installation
+
+Composer Update
+
+    composer update
+
+Create env file and set up your DB connection
+
+    cp .env.example .env
+
+Generate key
+
+    php artisan key:generate
+
+
+Run migrations without admins/tanent
+
+    php artisan migrate
+
+
+## References
+- [Laravel 9](https://laravel.com)
+- [Laravel Breeze](https://laravel.com/docs/9.x/starter-kits#laravel-breeze)
+- [Tenancy](https://tenancyforlaravel.com/)
---
+++ app/Http/Controllers/TenantController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tenant;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+
+class TenantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('tenant.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tenant.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'name' => 'required|string|max:30',
+            'email' => 'required|email|max:30',
+            'domain_name' => 'required|string|max:30|unique:domains,domain',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $tenant = Tenant::create($validator);
+
+        $tenant->domains()->create([
+            'domain' => $validator['domain_name'].'.'.config('app.domain')
+        ]);
+
+        return redirect()->route('tenant.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tenant  $tenant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tenant $tenant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tenant  $tenant
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tenant $tenant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tenant  $tenant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tenant $tenant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tenant  $tenant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tenant $tenant)
+    {
+        //
+    }
+}
